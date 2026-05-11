7647e0f0c6fb2068a7dc35c89ee1baba0775698d
M Atif Sarwar <matifsarwar32@gmail.com>
2026-05-11 19:07:24 +0500

    ADD TestCase

---
+++ basic-android-kotlin-compose-training-tip-calculator-starter/app/src/androidTest/kotlin/com/testcase/TipCalculatorTest.kt
@@ -0,0 +1,16 @@
+package com.testcase
+import com.example.tiptime.calculateTip
+import java.text.NumberFormat
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class TipCalculatorTest {
+    @Test
+    fun calculateTip_20PercentNoRoundUp(){
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, false)
+        assertEquals(expectedTip, actualTip)
+    }
+}
